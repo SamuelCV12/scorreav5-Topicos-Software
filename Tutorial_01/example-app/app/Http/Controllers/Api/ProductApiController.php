f47471b853e38d626efdaf475f76d6cd553543df
SamuelCV12 <scorreav5@eafit.edu.co>
2026-09-16 13:07:56 -0500

    Tutorial numero 04 Terminado

---
+++ Tutorial_01/example-app/app/Http/Controllers/Api/ProductApiController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Product;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+
+class ProductApiController extends Controller
+{
+    public function index(): JsonResponse
+    {
+        $products = Product::all();
+        return response()->json($products, 200);
+    }
+
+    public function show(string $id): JsonResponse
+    {
+        $product = Product::findOrFail($id);
+        return response()->json($product, 200);
+    }
+
+    public function store(Request $request): JsonResponse
+    {
+        $request->validate([
+            'name' => 'required|string|max:255',
+            'price' => 'required|numeric|min:0',
+        ]);
+
+        $product = new Product();
+        $product->name = $request->input('name');
+        $product->price = $request->input('price');
+        $product->save();
+
+        return response()->json($product, 201);
+    }
+}
